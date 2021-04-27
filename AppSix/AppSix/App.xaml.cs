8f92c70d1e6fb9cf9d25222e2a404f2e64f0a198
DominicB-yte <53456554+DominicB-yte@users.noreply.github.com>
2021-04-27 19:35:12 +1200

    Initial commit

---
+++ AppSix/AppSix/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppSix
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=b1a7e191-92c9-49d1-8c53-f20aa2f94c79;",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppSix/AppSix/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Analytics;
+
+namespace AppSix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRing_Clicked(object sender, EventArgs e)
+        {
+            Analytics.TrackEvent("Call Button Clicked");
+            DisplayAlert("", "Calling...", "OK");
+        }
+
+        private void btnCrash_Clicked(object sender, EventArgs e)
+        {
+            Crashes.GenerateTestCrash();
+        }
+    }
+}
